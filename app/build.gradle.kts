41a606d0fcccae24cc88e1a5dd1a74bf9a6f05c2
l00030730 <lvwuda@honor.com>
2026-06-03 13:04:23 +0800

    fix: use KSP for Room, add theme resource, fix permissions for API 34+

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 }
 
 android {
@@ -29,5 +30,5 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite:2.14.0")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 }
